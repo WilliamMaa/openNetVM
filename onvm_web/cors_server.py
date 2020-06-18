74f0da904fc34c7ddd5558d9add00f8f0ea756d0
WilliamMaa <williamma1998@outlook.com>
2020-06-17 21:15:42 -0400

    Change Python Server to Python2

--- onvm_web/cors_server.py
+++ onvm_web/cors_server.py
@@ -1,19 +1,13 @@
 # Source: https://stackoverflow.com/questions/21956683/enable-access-control-on-simple-http-server
 
-#!/usr/bin/env python3
-from http.server import HTTPServer, BaseHTTPRequestHandler, test
-# import json
+#!/usr/bin/env python2
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import BaseHTTPServer
 
-class CORSRequestHandler(BaseHTTPRequestHandler):
-    def end_headers(self):
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        BaseHTTPRequestHandler.end_headers(self)
-
-    # def do_POST(self):
-    #     post_body = self.rfile.read(int(self.headers['Content-Length']))
-    #     print(post_body)
-    #     post_body_json = json.loads(post_body);
-    #     print(post_body_json)
+        SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=8000)
+    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
