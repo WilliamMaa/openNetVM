c04fa2a30de674461dc9974986111f73e6e7b06b
WilliamMaa <williamma1998@outlook.com>
2020-06-19 13:56:43 -0400

    Test web post

--- onvm_web/react-app/src/pages/NFChain.react.js
+++ onvm_web/react-app/src/pages/NFChain.react.js
@@ -17,12 +17,7 @@ class NFChain extends React.Component {
         this.state = {
             request_type: "start"
         }
-        axios.post(`http://${hostName}:8000`, this.state，
-            {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(response => {
+        axios.post(`http://${hostName}:8000`, this.state).then(response => {
                 console.log(response)
             }).catch(error => {
                 console.log(error)
