ee06212fc4b147ee938a654cfd77d608e7e28ea4
WilliamMaa <williamma1998@outlook.com>
2020-06-23 03:00:38 -0400

    Test printing nf stats

--- examples/config.py
+++ examples/config.py
@@ -151,6 +151,7 @@ if __name__ == '__main__':
                 universal_newlines=True)
             procs_list.append(p)
             print("Starting %s %s" % (nf, cmd))
+            print("Pid %d" % (p.pid))
             i += 1
         except OSError:
             pass
