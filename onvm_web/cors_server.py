a4143a44170fa0a366f3393eb34566a024f3f0f5
WilliamMaa <williamma1998@outlook.com>
2020-07-04 13:36:23 -0400

    Test

--- onvm_web/cors_server.py
+++ onvm_web/cors_server.py
@@ -97,8 +97,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
                 while(log is not None and log != ""):
                     log_info = log.split(" ")
                     if(log_info[0] == "Starting"):
-                        # command = 'ps -ef | grep sudo | grep speed_tester | grep -v "grep" | awk "{print $2}"'
-                        command = 'echo "hello world"'
+                        command = "ps -ef | grep sudo | grep speed_tester | grep -v 'grep' | awk '{print $2}'"
                         pids = os.Popen(shlex.split(command), stdout=(close_log_file), stderr=close_log_file, universal_newlines=True)
                         pid_processes = pids.read()
                         if pid_processes != "":
