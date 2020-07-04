6604c397fdece9a034e2087c564741c98db7455d
WilliamMaa <williamma1998@outlook.com>
2020-07-04 13:24:43 -0400

    Test output

--- onvm_web/cors_server.py
+++ onvm_web/cors_server.py
@@ -6,6 +6,7 @@ import json
 import subprocess
 import os
 import signal
+import shelx
 
 global is_running
 is_running = -1
@@ -90,12 +91,14 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         message = []
         try:
             # open the log file to read the is_runnings of the nfs
+            close_log_file=open('./log.txt', 'w+')
             with open('./test.txt', 'r') as log_file:
                 log = log_file.readline()
                 while(log is not None and log != ""):
                     log_info = log.split(" ")
                     if(log_info[0] == "Starting"):
-                        pids = os.popen('ps -ef | grep sudo | grep ' + log_info[1] + ' | grep -v "grep" | awk "{print $2}"')
+                        command = 'ps -ef | grep sudo | grep ' + log_info[1] + ' | grep -v "grep" | awk "{print $2}"'
+                        pids = os.Popen(shelx.split(command), stdout=(close_log_file), stderr=close_log_file, universal_newlines=True)
                         pid_processes = pids.read()
                         if pid_processes != "":
                             pid_processes = pid_processes.split("\n")
