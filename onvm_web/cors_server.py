782dd313c56ae00eb69b93719173508da4aecfb6
WilliamMaa <williamma1998@outlook.com>
2020-07-04 13:49:12 -0400

    Test

--- onvm_web/cors_server.py
+++ onvm_web/cors_server.py
@@ -94,6 +94,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
             close_log_file = open('./log.txt', 'w+')
             with open('./test.txt', 'r') as log_file:
                 log = log_file.readline()
+                message.append("hello")
                 while(log is not None and log != ""):
                     log_info = log.split(" ")
                     message.append(log_info[0])
