c5bc9deb64f57043e108be1eecd630f248fe8277
WilliamMaa <williamma1998@outlook.com>
2020-07-05 21:50:52 -0400

    Adding checks fix issues

--- onvm_web/cors_server.py
+++ onvm_web/cors_server.py
@@ -8,6 +8,9 @@ import os
 import signal
 import cgi
 
+# use this variable to track if the nf is still running
+# if the nf chain is running, 1
+# if the nf chain is not running, -1
 global is_running
 is_running = -1
 
@@ -25,17 +28,14 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     # handle post event
     def do_POST(self):
         # if request type is form-data
-        if(self.headers.get('content-type').split(";")[0] == 'multipart/form-data'):
-
+        if self.headers.get('content-type').split(";")[0] == 'multipart/form-data':
             form = cgi.FieldStorage(
                 fp=self.rfile,
                 headers=self.headers,
                 environ={'REQUEST_METHOD': 'POST',
-                        'CONTENT_TYPE': self.headers['Content-Type'],
-                        }
+                         'CONTENT_TYPE': self.headers['Content-Type'],
+                         }
             )
-            self.send_response(200)
-            self.end_headers()
             for field in form.keys():
                 field_item = form[field]
                 # filename = field_item.filename
@@ -43,6 +43,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
                 # filesize = len(filevalue)
                 with open("../examples/nf_chain_config.json", 'w+') as f:
                     f.write(str(filevalue, 'utf-8'))
+            self.send_message(200)
             return None
 
         # get request body and parse it into json format
@@ -54,12 +55,9 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
             request_type = post_body_json['request_type']
         except KeyError:
             # if the body does not have request_type field
-            self.send_response(500)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
             response = json.dumps(
                 {'status': '500', 'message': 'missing request type'})
-            self.wfile.write(str.encode(response))
+            self.send_message(500, response)
             return None
 
         # handle start nf chain requests
@@ -68,6 +66,10 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         # handle stop nf chain request
         elif request_type == "stop":
             self.stop_nf_chain()
+        else:
+            response = json.dumps(
+                {'status': '403', 'message': 'unknown request'})
+            self.send_message(403, response)
 
     # handle options request
     def do_OPTIONS(self):
@@ -77,55 +79,55 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     # handle start NF chain event
     def start_nf_chain(self):
         global is_running
-        command = ['python3', '../examples/config.py',
-                   '../examples/nf_chain_config.json']
+        # check if the process is already started
+        is_running = self.check_is_running()
+        if is_running == 1:
+            response = json.dumps(
+                {'status': '403', 'message': 'process already started'})
+            self.send_message(403, response)
+            return None
         try:
-            # check if the process is already started
-            if is_running != -1:
-                self.send_response(403)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                response = json.dumps(
-                    {'status': '403', 'message': 'process already started'})
-                self.wfile.write(str.encode(response))
-                # setup return result
-                return -1
-
+            command = ['python3', '../examples/config.py',
+                       '../examples/nf_chain_config.json']
             # start the process and change the state
-            log_file = open('./test.txt', 'w+')
+            log_file = open('./log.txt', 'w+')
             os.chdir('../examples/')
-            p = subprocess.Popen(command, stdout=(
-                log_file), stderr=log_file, universal_newlines=True)
+            p = subprocess.Popen(command, stdout=log_file,
+                                 stderr=log_file, universal_newlines=True)
             is_running = 1
             # send success message
-            self.send_response(200)
+            response_code = 200
             response = json.dumps(
-                {'status': '200', 'message': 'success starting nfs', 'pid': str(p.pid)})
+                {'status': '200', 'message': 'start nfs successed', 'pid': str(p.pid)})
             # change back to web dir for correct output
             os.chdir('../onvm_web/')
-            # setup return result
-            result = 0
         except OSError:
-            self.send_response(500)
+            response_code = 500
             response = json.dumps(
-                {'status': '500', 'message': 'failed starting nfs'})
-            # setup return result
-            result = -1
+                {'status': '500', 'message': 'start nfs failed'})
         finally:
             log_file.close()
-            self.end_headers()
-            self.wfile.write(str.encode(response))
-        return result
+            self.send_message(response_code, response)
 
+    # handle stop nf request
     def stop_nf_chain(self):
         global is_running
         try:
+            # check if the process is already stopped
+            is_running = self.check_is_running()
+            if is_running == 1:
+                response = json.dumps(
+                    {'status': '500', 'message': 'nfs already stoped'})
+                self.send_message(500, response)
+                is_running = -1
+                return None
             # open the log file to read the process name of the nfs
-            with open('./test.txt', 'r') as log_file:
+            with open('./log.txt', 'r') as log_file:
                 log = log_file.readline()
-                while(log is not None and log != ""):
+                while log is not None and log != "":
                     log_info = log.split(" ")
-                    if(log_info[0] == "Starting"):
+                    if log_info[0] == "Starting":
+                        # get the pids of the nfs
                         command = "ps -ef | grep sudo | grep " + \
                             log_info[1] + \
                             " | grep -v 'grep' | awk '{print $2}'"
@@ -135,23 +137,45 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
                             pid_processes = pid_processes.split("\n")
                             for i in pid_processes:
                                 if i != "":
-                                    os.kill(int(i), signal.SIGKILL)
+                                    os.kill(i, signal.SIGKILL)
                     log = log_file.readline()
             # reset is_running
             is_running = -1
-            self.send_response(200)
+            response_code = 200
             response = json.dumps(
                 {'status': '200', 'message': 'stop nfs successed'})
-            result = 0
         except OSError:
-            self.send_response(500)
+            response_code = 500
             response = json.dumps(
-                {'status': '500', 'message': 'failed stopping nfs'})
-            result = -1
+                {'status': '500', 'message': 'stop nfs failed'})
         finally:
-            self.end_headers()
-            self.wfile.write(str.encode(response))
-        return result
+            self.clear_log()
+            self.send_message(response_code, response)
+
+    # send response message
+    def send_message(self, response_code, message=None):
+        self.send_response(response_code)
+        self.end_headers()
+        if message is not None:
+            self.wfile.write(str.encode(message))
+
+    # check if the nf is running
+    def check_is_running(self):
+        with open('./log.txt', 'r') as log_file:
+            log = log_file.readline()
+            if log is None or log == "":
+                return 1
+            while log is not None and log != "":
+                log_info = log.split(" ")
+                if log_info[0] == "Error" or log_info[0] == "Exiting...":
+                    return 1
+        return -1
+
+    # clear output log
+    def clear_log(self):
+        with open('./log.txt', 'r+') as log_file:
+            log_file.seek(0)
+            log_file.truncate()
 
 
 if __name__ == '__main__':
