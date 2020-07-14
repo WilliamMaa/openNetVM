305f78a50a6b71f6d5c32c7054c4eeca07985a72
WilliamMaa <williamma1998@outlook.com>
2020-07-14 16:04:46 -0400

    Alert error

--- onvm_web/react-app/src/pages/LaunchNFChain.react.js
+++ onvm_web/react-app/src/pages/LaunchNFChain.react.js
@@ -32,7 +32,7 @@ class LaunchNFChainPage extends Component {
       })
       .catch(error => {
         console.log(error);
-        // alert(error.json());
+        alert(error);
       });
 
     this.setState({
@@ -64,7 +64,7 @@ class LaunchNFChainPage extends Component {
       })
       .catch(error => {
         console.log(error);
-        // alert(error.json());
+        alert(error);
       });
   };
 
@@ -81,7 +81,7 @@ class LaunchNFChainPage extends Component {
       })
       .catch(error => {
         console.log(error);
-        // alert(error.json());
+        alert(error);
       });
     this.setState({
       launch: 1
